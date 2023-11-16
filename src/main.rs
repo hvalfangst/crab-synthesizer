3a57727641081928795033f8002ab5bed7379380
hvalfangst <hvalfangst@tidehunter.eu>
2023-11-16 23:03:55 +0100

    Implemented Wavetable Oscillator with supersaw

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::thread;
 use std::time::Duration;
 use console::{Key, Term};
 use rodio::{OutputStream, Sink};
-use rodio::source::{Amplify, SineWave, Source, TakeDuration};
+use rodio::source::Source;
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct State {
     pub octave: i32
 }
@@ -33,28 +33,100 @@ impl Note {
             Note::G => 783.99,
         };
 
-        // Calculate the frequency for the specified octave by adjusting the base frequency
-        // with a factor determined by raising 2.0 to the power of (state.octave - 4).
-        base_frequency * 2.0_f32.powi(state.octave - 4)
+        // Calculate the frequency for the specified octave by adjusting the base frequency with a factor determined by raising 2.0 to the power of (state.octave - 4).
+        let octave_adjusted_frequency = base_frequency * 2.0_f32.powi(state.octave - 4);
+        println!("Octave adjusted frequency {:?}", octave_adjusted_frequency);
+        octave_adjusted_frequency
     }
 }
 
-fn get_sine_wave(note: Note, duration: f32, amplitude: f32, state: State) -> Amplify<TakeDuration<SineWave>> {
-    SineWave::new(note.frequency(state))
-        .take_duration(Duration::from_secs_f32(duration))
-        .amplify(amplitude)
+#[derive(Clone)]
+struct WavetableOscillator {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+
+
+impl WavetableOscillator {
+    fn new(sample_rate: u32, wave_table: Vec<f32>) -> WavetableOscillator {
+        return WavetableOscillator {
+            sample_rate,
+            wave_table,
+            index: 0.0,
+            index_increment: 0.0,
+        };
+    }
+    fn set_frequency(&mut self, frequency: f32) {
+        self.index_increment = frequency * self.wave_table.len() as f32 / self.sample_rate.clone() as f32;
+    }
+    fn get_sample(&mut self) -> f32 {
+        let sample = self.lerp();
+        self.index += self.index_increment.clone();
+        self.index %= self.wave_table.len() as f32;
+        return sample;
+    }
+
+    fn lerp(&self) -> f32 {
+        let truncated_index = self.index.clone() as usize;
+        let next_index = (truncated_index + 1) % self.wave_table.len();
+        let next_index_weight = self.index.clone() - truncated_index.clone() as f32;
+        let truncated_index_weight = 1.0 - next_index_weight;
+        return truncated_index_weight * self.wave_table[truncated_index.clone()].clone() + next_index_weight.clone() * self.wave_table[next_index].clone();
+    }
+}
+
+impl Iterator for WavetableOscillator {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        return Some(self.get_sample());
+    }
+}
+
+impl Source for WavetableOscillator {
+    fn current_frame_len(&self) -> Option<usize> {
+        return None;
+    }
+
+    fn channels(&self) -> u16 {
+        return 1;
+    }
+
+    fn sample_rate(&self) -> u32 {
+        return self.sample_rate.clone();
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        return None;
+    }
 }
 
 fn main() {
+    let wave_table_size = 64;
+    let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
+
+
+    for n in 0..wave_table_size {
+        let mut sample = 0.0;
+
+        for a in 1..=5 {
+            let detune_factor = 0.25; d
+            let frequency = 220.0 * (a as f32).exp2() * (1.0 + detune_factor * (a.clone() as f32 - 1.0));
+            let amplitude = 1.0 / a.clone() as f32;
+            sample += amplitude * (2.0 * frequency * (n.clone() as f32) / wave_table_size.clone() as f32).sin();
+        }
+
+        wave_table.push(sample);
+    }
+
     let mut state = State{octave: 4};
-    let waveform = WaveForm{frequency: 440.0, duration: 0.25, amplitude: 0.20};
+    let mut oscillator = WavetableOscillator::new(44100, wave_table);
+    oscillator.set_frequency(Note::A.frequency(state));
 
     let term = Term::stdout();
-
-    // Create an output stream and a handle to control the stream
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-
-    // Create a sink in a separate thread to play audio
     let sink = Sink::try_new(&stream_handle).unwrap();
 
     loop {
@@ -79,16 +151,20 @@ fn main() {
                     _ => panic!("Unexpected key"),
                 };
 
-                println!("Note {:?}, Octave {:?}", note, state.octave);
-                sink.append(get_sine_wave(note, waveform.clone().duration, waveform.clone().amplitude, state.clone()));
+               println!("Note {:?}, Octave {:?}", note, state.clone().octave);
+                oscillator.set_frequency(note.frequency(state.clone()));
+                let cloned_oscillator = oscillator.clone();
+
+                let sound_source = cloned_oscillator.take_duration(Duration::from_secs_f32(0.25)).convert_samples::<f32>();
+                let _result = sink.append(sound_source);
             }
             Key::Char('o') | Key::Char('O') => {
-                let new_octave = state.octave -1;
+                let new_octave = state.octave.clone() -1;
                 println!("Octave has been reduced from {:?} to {:?}", state.octave, new_octave);
                 state.octave = new_octave;
             }
             Key::Char('p') | Key::Char('P') => {
-                let new_octave = state.octave +1;
+                let new_octave = state.octave.clone() +1;
                 println!("Octave has been increased from {:?} to {:?}", state.octave, new_octave);
                 state.octave = new_octave;
             }
@@ -97,10 +173,10 @@ fn main() {
                 break;
             }
             _ => {
-                println!("Invalid key. Press 'Z' to play the sound. Press 'Q' to quit.");
+                println!("Invalid key. Press 'QWERTY' to play, 'O/P' to modify octave and  'Z' to quit.");
             }
         }
 
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(50));
     }
 }
